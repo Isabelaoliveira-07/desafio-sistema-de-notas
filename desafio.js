12dc86ad2614c5e9dab5c06a584b6bd464cd7f38
Isabelaoliveira-07 <isabela.o.pereira2edu.senai.br>
2026-08-27 08:00:56 -0300

    first commit

--- desafio.js
+++ desafio.js
@@ -1,6 +1,6 @@
-function calculadorMaiorNota(nota1 , nota2) {
+function calcularMedia(nota1, nota2) {
     return (nota1 + nota2) / 2;
-}   
+}
 
 function verificarSituacao(media) {
     if (media >= 7) {
@@ -11,31 +11,21 @@ function verificarSituacao(media) {
         return "Reprovado";
     }
 }
- function mostrarResultado(nome, media, situacao) {
+
+function mostrarResultado(nome, media, situacao) {
     console.log("Aluno: " + nome);
-    console.log("Média: " + media.toFixed(2));
+    console.log("Média: " + media);
     console.log("Situação: " + situacao);
-
 }
-function calcularMaiorNota  (nota1, nota2) {
-    if (nota1 > nota2) {
-        return nota1;
-    } else {
-        return nota2;       
-    }
-}
-let nome=prompt("Digite o nome do aluno:");
-let nota1= 8
-let nota2= 6
-let media = calculadorMaiorNota(nota1, nota2);
-let situacao = verificarSituacao(media);
-mostrarResultado(nome, media, situacao);
-console.log("Maior nota: " + calcularMaiorNota(nota1, nota2));
 
-let nome2=prompt("Digite o nome do aluno:");
-let nota3= 9
-let nota4= 5
-let media = calcularMedia(nota3, nota4);
-let situacao = verificarSituacao(media);
-mostrarResultado(nome2, media, situacao);
-console.log("Maior nota: " + calcularMaiorNota(nota3, nota4));
+let media1 = calcularMedia(8, 6);
+let situacao1 = verificarSituacao(media1);
+mostrarResultado("Joãozinho", media1, situacao1);
+
+let media2 = calcularMedia(5, 6);
+let situacao2 = verificarSituacao(media2);
+mostrarResultado("Maria", media2, situacao2);
+
+let media3 = calcularMedia(3, 4);
+let situacao3 = verificarSituacao(media3);
+mostrarResultado("Cleusa Maria", media3, situacao3);
